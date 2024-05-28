3e15628adfbaeb3461ebca81e7ed3c1aea2a7429
Mikhail <ashurmetovmm@gmail.com>
2024-05-28 16:08:12 +0300

    added absolute path in tests

--- deadlock-test-suite/tt/tte.py
+++ deadlock-test-suite/tt/tte.py
@@ -2,10 +2,11 @@ import unittest
 import subprocess
 import os
 
+
 class TestFlake8(unittest.TestCase):
     def test_flake8_dl_acquiring_in_wrong_order(self):
         current_dir = os.getcwd()
-        filepath = "../testcases/CWE833_Deadlock/dl_acquiring_in_wrong_order.py"
+        filepath = "/home/runner/work/flake8/flake8/deadlock-test-suite/testcases/CWE833_Deadlock/dl_acquiring_in_wrong_order.py"
         command = ["flake8", "--select=DLC001", filepath]
 
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -16,7 +17,7 @@ class TestFlake8(unittest.TestCase):
 
     def test_flake8_dl_in_class(self):
         current_dir = os.getcwd()
-        filepath = "../testcases/CWE833_Deadlock/dl_in_class.py"
+        filepath = "/home/runner/work/flake8/flake8/deadlock-test-suite/testcases/CWE833_Deadlock/dl_in_class.py"
         command = ["flake8", "--select=DLC001", filepath]
 
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -27,7 +28,7 @@ class TestFlake8(unittest.TestCase):
 
     def test_flake8_dl_in_decorator(self):
         current_dir = os.getcwd()
-        filepath = "../testcases/CWE833_Deadlock/dl_in_decorator.py"
+        filepath = "/home/runner/work/flake8/flake8/deadlock-test-suite/testcases/CWE833_Deadlock/dl_in_decorator.py"
         command = ["flake8", "--select=DLC001", filepath]
 
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -38,7 +39,7 @@ class TestFlake8(unittest.TestCase):
 
     def test_flake8_dl_on_condition(self):
         current_dir = os.getcwd()
-        filepath = "../testcases/CWE833_Deadlock/dl_on_condition.py"
+        filepath = "/home/runner/work/flake8/flake8/deadlock-test-suite/testcases/CWE833_Deadlock/dl_on_condition.py"
         command = ["flake8", "--select=DLC001", filepath]
 
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -49,7 +50,7 @@ class TestFlake8(unittest.TestCase):
 
     def test_flake8_dl_on_event(self):
         current_dir = os.getcwd()
-        filepath = "../testcases/CWE833_Deadlock/dl_on_event.py"
+        filepath = "/home/runner/work/flake8/flake8/deadlock-test-suite/testcases/CWE833_Deadlock/dl_on_event.py"
         command = ["flake8", "--select=DLC001", filepath]
 
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -60,7 +61,7 @@ class TestFlake8(unittest.TestCase):
 
     def test_flake8_dl_on_event_and_condition(self):
         current_dir = os.getcwd()
-        filepath = "../testcases/CWE833_Deadlock/dl_on_event_and_condition.py"
+        filepath = "/home/runner/work/flake8/flake8/deadlock-test-suite/testcases/CWE833_Deadlock/dl_on_event_and_condition.py"
         command = ["flake8", "--select=DLC001", filepath]
 
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -71,7 +72,7 @@ class TestFlake8(unittest.TestCase):
 
     def test_flake8_dl_recursive(self):
         current_dir = os.getcwd()
-        filepath = "../testcases/CWE833_Deadlock/dl_recursive.py"
+        filepath = "/home/runner/work/flake8/flake8/deadlock-test-suite/testcases/CWE833_Deadlock/dl_recursive.py"
         command = ["flake8", "--select=DLC001", filepath]
 
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -82,7 +83,7 @@ class TestFlake8(unittest.TestCase):
 
     def test_flake8_dl_wait_on_each(self):
         current_dir = os.getcwd()
-        filepath = "../testcases/CWE833_Deadlock/dl_wait_on_each.py"
+        filepath = "/home/runner/work/flake8/flake8/deadlock-test-suite/testcases/CWE833_Deadlock/dl_wait_on_each.py"
         command = ["flake8", "--select=DLC001", filepath]
 
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -93,7 +94,7 @@ class TestFlake8(unittest.TestCase):
 
     def test_flake8_dl_waits_on_itself(self):
         current_dir = os.getcwd()
-        filepath = "../testcases/CWE833_Deadlock/dl_waits_on_itself.py"
+        filepath = "/home/runner/work/flake8/flake8/deadlock-test-suite/testcases/CWE833_Deadlock/dl_waits_on_itself.py"
         command = ["flake8", "--select=DLC001", filepath]
 
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -104,7 +105,7 @@ class TestFlake8(unittest.TestCase):
 
     def test_flake8_dl_release_lock_fail(self):
         current_dir = os.getcwd()
-        filepath = "../testcases/CWE667_Improper_Locking/dl_release_lock_fail.py"
+        filepath = "/home/runner/work/flake8/flake8/deadlock-test-suite/testcases/CWE667_Improper_Locking/dl_release_lock_fail.py"
         command = ["flake8", "--select=DLC001", filepath]
 
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -112,6 +113,7 @@ class TestFlake8(unittest.TestCase):
         output = output.decode('utf-8')
 
         self.assertIn("DLC001 potential deadlock detected", output)
+
 
 if __name__ == '__main__':
     unittest.main()
