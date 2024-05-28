a420c07e04c3e40ce4ef38168c51dd86cffecbc8
Mikhail <ashurmetovmm@gmail.com>
2024-05-28 05:39:46 +0300

    added identification for: - dl_on_condition - dl_on_event - dl_on_event_and_condition - dl_acquiring_in_wrong_order - dl_in_class - dl_in_decorator - dl_recursive - dl_waits_on_itself

--- src/flake8/deadlock_checker.py
+++ src/flake8/deadlock_checker.py
@@ -31,54 +31,57 @@ class DeadlockChecker:
 
     def build_graphs(self):
         for node in ast.walk(self.tree):
-            if isinstance(node, ast.FunctionDef):
-                func_name = node.name
-                if func_name in self.threads:
-                    current_locks = []
-                    current_events = []
-                    current_joins = []
-                    for n in ast.walk(node):
-                        if isinstance(n, ast.With):
-                            for item in n.items:
-                                lock_name = self.get_lock_name(item.context_expr)
-                                if lock_name:
-                                    if lock_name in [l[0] for l in current_locks]:
-                                        self.add_to_graph(self.lock_graph, (lock_name, n.lineno, n.col_offset), (lock_name, n.lineno, n.col_offset))
-                                    else:
+            if isinstance(node, ast.FunctionDef) or isinstance(node, ast.ClassDef):
+                for func_node in ast.walk(node):
+                    if isinstance(func_node, ast.FunctionDef):
+                        func_name = func_node.name
+                        current_locks = []
+                        current_events = []
+                        current_joins = []
+                        for n in ast.walk(func_node):
+                            if isinstance(n, ast.With):
+                                for item in n.items:
+                                    lock_name = self.get_lock_name(item.context_expr)
+                                    if lock_name:
+                                        if lock_name in [l[0] for l in current_locks]:
+                                            self.add_to_graph(self.lock_graph, (lock_name, n.lineno, n.col_offset), (lock_name, n.lineno, n.col_offset))
+                                        else:
+                                            current_locks.append((lock_name, n.lineno, n.col_offset))
+                                            self.add_to_graph(self.lock_graph, current_locks[-1], current_locks[-1])
+                            if isinstance(n, ast.Call) and isinstance(n.func, ast.Attribute):
+                                lock_name = self.get_lock_name(n.func.value)
+                                process_name = self.get_process_name(n.func.value)
+                                if n.func.attr in {'acquire', 'wait'}:
+                                    if lock_name:
                                         current_locks.append((lock_name, n.lineno, n.col_offset))
-                                        self.add_to_graph(self.lock_graph, current_locks[-1], current_locks[-1])
-                        if isinstance(n, ast.Call) and isinstance(n.func, ast.Attribute):
-                            lock_name = self.get_lock_name(n.func.value)
-                            process_name = self.get_process_name(n.func.value)
-                            if n.func.attr in {'acquire', 'wait'}:
-                                if lock_name:
-                                    current_locks.append((lock_name, n.lineno, n.col_offset))
-                                    if len(current_locks) > 1:
-                                        self.add_to_graph(self.lock_graph, current_locks[-2], current_locks[-1])
-                            elif n.func.attr in {'release', 'set'}:
-                                if lock_name in [l[0] for l in current_locks]:
-                                    current_locks = [l for l in current_locks if l[0] != lock_name]
-                            elif n.func.attr == 'wait':
-                                event_name = self.get_event_name(n.func.value)
-                                if event_name:
-                                    current_events.append((event_name, n.lineno, n.col_offset))
-                                    if len(current_events) > 1:
-                                        self.add_to_graph(self.event_graph, current_events[-2], current_events[-1])
-                            elif n.func.attr == 'set':
-                                event_name = self.get_event_name(n.func.value)
-                                if event_name in [l[0] for l in current_events]:
-                                    current_events = [l for l in current_events if l[0] != event_name]
-                            elif n.func.attr == 'join':
-                                if process_name:
-                                    current_joins.append((process_name, n.lineno, n.col_offset))
-                                    if len(current_joins) > 1:
-                                        self.add_to_graph(self.join_graph, current_joins[-2], current_joins[-1])
-                                    if len(current_joins) > 1 and current_joins[-1][0] == current_joins[-2][0]:
-                                        self.add_to_graph(self.join_graph, current_joins[-1], current_joins[-2])
+                                        if len(current_locks) > 1:
+                                            self.add_to_graph(self.lock_graph, current_locks[-2], current_locks[-1])
+                                elif n.func.attr in {'release', 'set'}:
+                                    if lock_name in [l[0] for l in current_locks]:
+                                        current_locks = [l for l in current_locks if l[0] != lock_name]
+                                elif n.func.attr == 'wait':
+                                    event_name = self.get_event_name(n.func.value)
+                                    if event_name:
+                                        current_events.append((event_name, n.lineno, n.col_offset))
+                                        if len(current_events) > 1:
+                                            self.add_to_graph(self.event_graph, current_events[-2], current_events[-1])
+                                elif n.func.attr == 'set':
+                                    event_name = self.get_event_name(n.func.value)
+                                    if event_name in [l[0] for l in current_events]:
+                                        current_events = [l for l in current_events if l[0] != event_name]
+                                elif n.func.attr == 'join':
+                                    if process_name:
+                                        current_joins.append((process_name, n.lineno, n.col_offset))
+                                        if len(current_joins) > 1:
+                                            self.add_to_graph(self.join_graph, current_joins[-2], current_joins[-1])
+                                        if len(current_joins) > 1 and current_joins[-1][0] == current_joins[-2][0]:
+                                            self.add_to_graph(self.join_graph, current_joins[-1], current_joins[-2])
 
     def get_lock_name(self, node):
         if isinstance(node, ast.Name):
             return node.id
+        elif isinstance(node, ast.Attribute) and isinstance(node.value, ast.Name):
+            return f"{node.value.id}.{node.attr}"
         return None
 
     def get_event_name(self, node):
@@ -89,6 +92,8 @@ class DeadlockChecker:
     def get_process_name(self, node):
         if isinstance(node, ast.Name):
             return node.id
+        elif isinstance(node, ast.Attribute) and isinstance(node.value, ast.Name):
+            return f"{node.value.id}.{node.attr}"
         return None
 
     def add_to_graph(self, graph, node1, node2):
@@ -136,4 +141,4 @@ def check_directory(directory):
                 check_file(filepath)
 
 # Пример запуска проверки для всех файлов в директории CWE833_Deadlock
-check_directory(r'C:\Users\killa\PycharmProjects\flake8\deadlock-test-suite\testcases\CWE833_Deadlock')
+# check_directory(r'C:\Users\killa\PycharmProjects\flake8\deadlock-test-suite\testcases\CWE833_Deadlock')
